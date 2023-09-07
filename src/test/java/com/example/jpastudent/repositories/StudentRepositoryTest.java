7f2c51995e9f376040ac9267a6bdebdf42797744
Enes Kocer <enes0539@stud.kea.dk>
2023-09-07 13:39:28 +0200

    test

--- src/test/java/com/example/jpastudent/repositories/StudentRepositoryTest.java
+++ src/test/java/com/example/jpastudent/repositories/StudentRepositoryTest.java
@@ -1,8 +1,10 @@
 package com.example.jpastudent.repositories;
 
 import com.example.jpastudent.model.Student;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
@@ -10,17 +12,24 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@DataJpaTest
 class StudentRepositoryTest {
 
     @Autowired
     StudentRepository studentRepository;
 
-    @Test
-    void testOnTime() {
-        List<Student> lst = studentRepository.findAllByName("Tim");
-        assertEquals(1, lst.size());
+    @BeforeEach
+    void setUp() {
+        Student student = new Student();
+        student.setName("test");
+        studentRepository.save(student);
     }
+
+//    @Test
+//    void testOnTime() {
+//        List<Student> lst = studentRepository.findAllByName("Tim");
+//        assertEquals(1, lst.size());
+//    }
 
     @Test
     void testOneViggo() {
@@ -29,6 +38,12 @@ class StudentRepositoryTest {
         s1.setBornDate(LocalDate.of(2010, 11, 12));
         studentRepository.save(s1);
         List<Student> lst = studentRepository.findAllByName("Viggo");
+        assertEquals(1, lst.size());
+    }
+
+    @Test
+    void testOneStudent() {
+        List<Student> lst = studentRepository.findAll();
         assertEquals(1, lst.size());
     }
 
