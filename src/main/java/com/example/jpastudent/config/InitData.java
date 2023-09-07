f276a8b847dbf4aafd23cd0a52d8934af0a30b91
Enes Kocer <enes0539@stud.kea.dk>
2023-09-07 13:27:07 +0200

    Initial commit

---
+++ src/main/java/com/example/jpastudent/config/InitData.java
@@ -0,0 +1,35 @@
+package com.example.jpastudent.config;
+
+import com.example.jpastudent.model.Student;
+import com.example.jpastudent.repositories.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Component
+public class InitData implements CommandLineRunner {
+
+    @Autowired
+    StudentRepository studentRepository;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        Student s1 = new Student();
+        s1.setName("Bruce");
+        s1.setBornDate(LocalDate.of(2010, 11, 22));
+        s1.setBornTime(LocalTime.of(10, 11, 12));
+        studentRepository.save(s1);
+        s1.setName("Tim");
+        studentRepository.save(s1);
+
+        Student s2 = new Student();
+        s2.setName("Lars");
+        s2.setBornDate(LocalDate.of(2000, 07, 12));
+        s2.setBornTime(LocalTime.of(10, 10, 10));
+        studentRepository.save(s2);
+    }
+}
---
+++ src/main/java/com/example/jpastudent/controller/StudentRestController.java
@@ -0,0 +1,78 @@
+package com.example.jpastudent.controller;
+
+import com.example.jpastudent.model.Student;
+import com.example.jpastudent.repositories.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class StudentRestController {
+
+    @Autowired
+    StudentRepository studentRepository;
+
+    @GetMapping("/students")
+    public List<Student> students() {
+        var obj = studentRepository.findAll();
+        return obj;
+    }
+
+    @GetMapping("/addstudent")
+    public List<Student> addStudent() {
+        Student std = new Student();
+        std.setBornDate(LocalDate.now());
+
+        List<Student> lst = studentRepository.findAll();
+        return lst;
+    }
+
+    @GetMapping("students/{name}")
+    public List<Student> getAllStudentsByName(@PathVariable String name) {
+        return studentRepository.findAllByName(name);
+    }
+
+//    @GetMapping("students/{id}")
+//    public List<Student> getAllStudentsByid(@PathVariable int id) {
+//        return studentRepository.findAllById(id);
+//    }
+
+    @PostMapping("/student/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student postStudent(@RequestBody Student student) {
+        System.out.println(student);
+        return studentRepository.save(student);
+    }
+
+    @PutMapping("/student/{id}")
+    public ResponseEntity<Student> putStudent(@PathVariable int id, @RequestBody Student student) {
+        Optional<Student> orgStudent = studentRepository.findById(id);
+        if (orgStudent.isPresent()) {
+            student.setId(id);
+            studentRepository.save(student);
+            return ResponseEntity.ok(student);
+            //return new ResponseEntity<>(student, HttpStatus.OK);
+        } else {
+            return ResponseEntity.notFound().build();
+            //return new ResponseEntity<>(new Student(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/student/{id}")
+    public ResponseEntity<String> deleteStudent(@PathVariable int id) {
+        Optional<Student> orgStudent = studentRepository.findById(id);
+        if (orgStudent.isPresent()) {
+            studentRepository.deleteById(id);
+            return ResponseEntity.ok("Student Deleted");
+        } else {
+            //return ResponseEntity.notFound().build();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Student Not Found");
+        }
+    }
+
+}
