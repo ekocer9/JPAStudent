c7e7ab841a09886c69d12b3e5edda67dc2c3e030
Enes Kocer <enes0539@stud.kea.dk>
2023-09-14 14:52:16 +0200

    fjernede ekstra test klasse

--- src/test/java/com/example/jpastudent/repositories/StudentRepositoryTest.java
+++ src/test/java/com/example/jpastudent/repositories/StudentRepositoryTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -14,4 +13,31 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class StudentRepositoryTest {
+
+    @Autowired
+    StudentRepository studentRepository;
+
+    @BeforeEach
+    void setUp() {
+        Student student = new Student();
+        student.setName("test");
+        studentRepository.save(student);
+    }
+
+    @Test
+    void testOneViggo() {
+        Student s1 = new Student();
+        s1.setName("Viggo");
+        s1.setBornDate(LocalDate.of(2010, 11, 12));
+        studentRepository.save(s1);
+        List<Student> lst = studentRepository.findAllByName("Viggo");
+        assertEquals(1, lst.size());
+    }
+
+    @Test
+    void testOneStudent() {
+        List<Student> lst = studentRepository.findAll();
+        assertEquals(1, lst.size());
+    }
+
 }
